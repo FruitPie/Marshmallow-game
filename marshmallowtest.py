6a8b55d27b1aaeadc0b4890444e78a7e6de0e0ba
Fruitpie <FruitPie@users.noreply.github.com>
2015-07-02 23:37:56 +0000

    Previous version stopped working. New & better version?
    Made some changes to the loops but working in the dark really as I don't fully understand them. Possible to cheat by holding the contacts together. Only works with cheap marshmallows! The good ones are too dense & it's almost impossible to make a contact.

--- marshmallowtest.py
+++ marshmallowtest.py
@@ -8,15 +8,21 @@ now = time.time()
 future = now + 10
 score = 1
 
-while time.time() < future:
+#while time.time() < future:
 
-	while True:
-		if GPIO.input(2) == False:
-			print(score)
-			time.sleep(0.5)
-			score = score + 1
-			break
+while True:
+	if GPIO.input(2) == False:
+		print(score)
+		time.sleep(0.5)
+		score = score + 1
+		if time.time() < future:
+			pass
+		#break
 	
-else:
-	#print (score + "points")
-	print("Game over!")
+		else:
+		#print (score + "points")
+		#while True:
+			if time.time() > future:
+				print("Game over!")
+				break
+			#end
